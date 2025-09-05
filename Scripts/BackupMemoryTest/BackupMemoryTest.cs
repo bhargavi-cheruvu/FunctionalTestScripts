f43c76139b52f693c9110daf69f4131bfa920b2e
bhargavi.cheruvu <bhargavi.cheruvu@TF-250718004506>
2025-09-05 16:59:13 +0530

    Added BackupMemory Functional Test to the Solution.

---
+++ Scripts/BackupMemoryTest/BackupMemoryTest.cs
@@ -0,0 +1,49 @@
+﻿using LogViewManager;
+using System;
+using UniversalBoardTestApp;
+using Helper;
+public class Test
+{
+    // Version of the script. Gets displayed in database/protocol
+    private const string TestVersion = Handler.TEST_VERSION; // Version Number
+    public bool Start()
+    {
+        if (ScriptHelper.CheckIfProcedureIsCancelled())
+            return false;
+
+        Logger.LogMessage(Level.Info, Handler.BackupMemoryTest);
+        if (CheckBackupMemoryConnected())
+            return true;
+        else
+            return false;
+    }
+
+    private bool CheckBackupMemoryConnected()
+    {
+        string resp;
+        // Backup Memory command
+        HardwareParameters.SetParameter(Handler.BACKUPMEMORY_COMMAND, Handler.REQUEST);
+        HardwareParameters.GetParameter(Handler.BACKUPMEMORY_COMMAND, out resp);
+
+        var lines = resp.Split(new[] { Handler.NEWLINE, Handler.CARRAIGE_RETURN }, StringSplitOptions.RemoveEmptyEntries);
+
+        foreach (string line in lines)
+        {
+            var token = line.Split(Handler.DELIMITER);
+            if (token.Length > Handler.INDEX_ZERO && token[Handler.INDEX_ZERO] == Handler.BACKUPMEMORY_STATUS)
+            {
+                string key = token[Handler.INDEX_ZERO];
+                resp = token[Handler.INDEX_ONE];
+                break;
+            }
+        }
+
+        if (resp == Handler.BackupStatusResponse)
+            return true;
+        else
+        {
+            Logger.LogMessage(Level.Error, Handler.BACKUPMEMORY_INVALID_RESPONSE);
+            return false;
+        }
+    }
+}
---
+++ Scripts/BackupMemoryTest/Helper.cs
@@ -0,0 +1,30 @@
+﻿using LogViewManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helper
+{
+    public class Handler
+    {
+        public const string TEST_VERSION = "0.0.0.1";
+        public const string BackupMemoryTest = "Starting Backup Memory Test Script.";
+        public const string BackupStatusResponse = "0,0,1";
+        public const char DELIMITER = '=';
+        public const string REQUEST = "?";
+
+        public const string BACKUPMEMORY_COMMAND= "BackupMemory.Status";
+        public const string BACKUPMEMORY_STATUS = "~BackupMemory.Status";
+
+        public const string BACKUPMEMORY_INVALID_RESPONSE = "Invalid Response or Backup memory is Disconnected";
+
+        public const int INDEX_ZERO = 0;
+        public const int INDEX_ONE = 1;
+
+        public const char NEWLINE = '\n';
+        public const char CARRAIGE_RETURN = '\r';
+    }
+}
