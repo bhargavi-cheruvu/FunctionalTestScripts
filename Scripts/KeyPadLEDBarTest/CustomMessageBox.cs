5b034e489571069168a8cdd01bca8a8d2b71a9cd
bhargavi.cheruvu <bhargavi.cheruvu@TF-250718004506>
2025-11-26 15:35:35 +0530

    Removed commented code

--- Scripts/KeyPadLEDBarTest/CustomMessageBox.cs
+++ Scripts/KeyPadLEDBarTest/CustomMessageBox.cs
@@ -1,222 +1,4 @@
-﻿//using Helper;
-//using LogViewManager;
-//using System;
-//using System.Collections.Generic;
-//using System.ComponentModel;
-//using System.Data;
-//using System.Drawing;
-//using System.Linq;
-//using System.Text;
-//using System.Threading;
-//using System.Threading.Tasks;
-//using System.Windows.Forms;
-//using UniversalBoardTestApp;
-
-//namespace KeyPadLEDBarTest
-//{
-//    public partial class CustomMessageBox : Form
-//    {
-//        private Label lblMessage;
-//        private FlowLayoutPanel buttonPanel;
-//        public string SelectedButton { get; private set; } = null;
-
-//        private static int KeyPressVal = 0;
-
-//        public CustomMessageBox()
-//        {
-//            InitializeComponent();
-//        }
-
-//        public CustomMessageBox(string title, string message, string[] buttons)
-//        {
-//            this.Text = title;
-//            this.StartPosition = FormStartPosition.CenterParent;
-//            this.Size = new Size(350, 180);
-//            this.FormBorderStyle = FormBorderStyle.FixedDialog;
-//            this.MinimizeBox = false;
-//            this.MaximizeBox = false;
-//            this.KeyPreview = true;
-
-//            this.KeyDown += CustomMessageBox_KeyDown;
-//            this.KeyUp += CustomMessageBox_KeyUp;
-
-//            lblMessage = new Label()
-//            {
-//                Text = message,
-//                AutoSize = false,
-//                Location = new Point(15, 15),
-//                Size = new Size(320, 60)
-//            };
-
-//            buttonPanel = new FlowLayoutPanel()
-//            {
-//                FlowDirection = FlowDirection.RightToLeft,
-//                Dock = DockStyle.Bottom,
-//                Height = 45
-//            };
-
-//            foreach (var btnText in buttons)
-//            {
-//                var btn = new Button()
-//                {
-//                    Text = btnText,
-//                    Width = 80,
-//                    Height = 30,
-//                    Margin = new Padding(5)
-//                };
-
-//                btn.Click += (s, e) =>
-//                {
-//                    SelectedButton = btn.Text;
-//                    this.DialogResult = DialogResult.OK;
-//                    this.Close();
-//                };
-//                buttonPanel.Controls.Add(btn);
-//            }
-
-//            this.Controls.Add(lblMessage);
-//            this.Controls.Add(buttonPanel);
-//        }
-
-
-//        public void TriggerKeyDown(Keys key)
-//        {
-//            KeyEventArgs args = new KeyEventArgs(key);
-//            this.OnKeyDown(args);
-//        }
-
-//        public void TriggerKeyUp(Keys key)
-//        {
-//            KeyEventArgs args = new KeyEventArgs(key);
-//            this.OnKeyUp(args);
-//        }
-
-//        private void CustomMessageBox_KeyDown(object sender, KeyEventArgs e)
-//        {
-//            // Hardware check (existing logic)
-//            HardwareParameters.GetParameter(Handler.RESPONSE_KEYPROPERTY, out string Val, true);
-//            if (Val != null && WaitForKeyPadExpectedResponse(Handler.RESPONSE_KEYPROPERTY, "1", 1000))
-//            {
-//                Console.WriteLine("MuteAlarm : " + KeyPressVal);
-//            }
-
-//            // Handle Enter → default button
-//            if (e.KeyCode == Keys.Enter && buttonPanel.Controls.Count > 0)
-//            {
-//                ((Button)buttonPanel.Controls.Cast<Button>().First()).PerformClick(); // First button as default
-//            }
-
-//            // Handle Escape → Cancel
-//            if (e.KeyCode == Keys.Escape)
-//            {
-//                SelectedButton = "Cancel";
-//                this.DialogResult = DialogResult.Cancel;
-//                this.Close();
-//            }
-
-//            // Handle Yes/No shortcuts
-//            if (e.KeyCode == Keys.Y)
-//            {
-//                var yesButton = buttonPanel.Controls.Cast<Button>().FirstOrDefault(b => b.Text.Equals("Yes", StringComparison.OrdinalIgnoreCase));
-//                yesButton?.PerformClick();
-//            }
-//            else if (e.KeyCode == Keys.N)
-//            {
-//                var noButton = buttonPanel.Controls.Cast<Button>().FirstOrDefault(b => b.Text.Equals("No", StringComparison.OrdinalIgnoreCase));
-//                noButton?.PerformClick();
-//            }
-//        }
-
-
-//        //private void CustomMessageBox_KeyDown(object sender, KeyEventArgs e)
-//        //{
-//        //    HardwareParameters.GetParameter(Handler.RESPONSE_KEYPROPERTY, out string Val, true);
-//        //    if (Val != null)
-//        //    {
-//        //        if (WaitForKeyPadExpectedResponse(Handler.RESPONSE_KEYPROPERTY, "1", 1000))
-//        //        {
-//        //            Console.WriteLine("MuteAlarm : " + KeyPressVal);
-//        //            //Console.WriteLine("Key Up: " + e.KeyCode);
-//        //        }
-//        //    }
-
-//        //    if (e.KeyCode == Keys.Enter && buttonPanel.Controls.Count > 0)
-//        //    {
-//        //        ((Button)buttonPanel.Controls.Cast<Button>().Last()).PerformClick();
-//        //    }
-
-//        //    if (e.KeyCode == Keys.Escape)
-//        //    {
-//        //        SelectedButton = "Cancel";
-//        //        this.DialogResult = DialogResult.Cancel;
-//        //        this.Close();
-//        //    }
-//        //}    
-
-//        private void CustomMessageBox_KeyUp(object sender, KeyEventArgs e)
-//        {
-//            HardwareParameters.GetParameter(Handler.RESPONSE_KEYPROPERTY, out string Val, true);
-//            if (Val != null)
-//            {
-//                if (WaitForKeyPadExpectedResponse(Handler.RESPONSE_KEYPROPERTY, "1", 1000))
-//                {
-//                    Console.WriteLine("MuteAlarm : " + KeyPressVal);
-//                    //Console.WriteLine("Key Up: " + e.KeyCode);
-//                }
-//            }
-//        }
-
-
-//        private bool WaitForKeyPadExpectedResponse(string parameterName, string expectedValue, int timeoutMs)
-//        {
-//            int elapsed = 0;
-//            int interval = 300;
-//            string response;
-//            int rVal = -1;
-
-//            while (elapsed < timeoutMs)
-//            {
-//                HardwareParameters.GetParameter(parameterName, out response, true);
-//                // Parse and Validate for Service Challenge and Service code values.           
-//                var lines = response.Split(new[] { Handler.NEWLINE, Handler.CARRAIGE_RETURN }, StringSplitOptions.RemoveEmptyEntries);
-
-//                foreach (string line in lines)
-//                {
-//                    var token = line.Split(Handler.DELIMITER);
-//                    if (token.Length > 0 && token[0] == Handler.KEYS_RESP_CMD)
-//                    {
-//                        string k = token[0];
-//                        response = token[1];
-//                        KeyPressVal = rVal = Convert.ToInt32(token[1]);
-//                        break;
-//                    }
-//                }
-
-//                if (!string.IsNullOrEmpty(response) && rVal == Convert.ToInt32(expectedValue))
-//                {
-//                    Logger.LogMessage(Level.Info, $"Response for KeyPad.Keys is {rVal}");
-//                    return true;
-//                }
-
-//                Thread.Sleep(interval);
-//                elapsed += interval;
-//            }
-
-//            return false;
-//        }
-
-//        //Static helper
-//        public static string Show(string title, string message, params string[] buttons)
-//        {
-//            using (var box = new CustomMessageBox(title, message, buttons))
-//            {
-//                box.ShowDialog();
-//                return box.SelectedButton;
-//            }
-//        }
-//    }
-//}
-using Helper;
+﻿using Helper;
 using LogViewManager;
 using System;
 using System.Drawing;
@@ -224,7 +6,6 @@ using System.Linq;
 using System.Threading;
 using System.Windows.Forms;
 using UniversalBoardTestApp;
-
 
 namespace KeyPadLEDBarTest
 {
@@ -240,12 +21,10 @@ namespace KeyPadLEDBarTest
         public CustomMessageBox(string title, string message, string[] buttons)
         {
             InitializeComponent();
-
             this.Text = title;
             this.StartPosition = FormStartPosition.CenterParent;
             this.Size = new Size(700, 350);
             this.KeyPreview = true;
-
             this.KeyDown += CustomMessageBox_KeyDown;
             this.FormClosing += CustomMessageBox_FormClosing;
 
@@ -324,7 +103,6 @@ namespace KeyPadLEDBarTest
                 this.Close();
             }
         }
-
         private void CustomMessageBox_KeyDown(object sender, KeyEventArgs e)
         {
             // Do NOT read hardware here. Only map keyboard to buttons.
@@ -356,7 +134,6 @@ namespace KeyPadLEDBarTest
                 }
             }
         }
-
         private bool ReadHardwareKey(out int val)
         {
             val = -1;
